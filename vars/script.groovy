4ab8518e7c7d6adaf99c642c899db66ad871f350
Ravi-s10 <49112786+Ravi-s10@users.noreply.github.com>
2023-12-29 18:30:06 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -3,12 +3,7 @@
 println "Hello , This is a groovy Script!"
 
 
-def printGreeting(name) {
-println "Greetings, $name"
-}
-
-printGreeting("Ravi")
-
+def call() {
 
 
 def result = "ls -ltr".excute()
@@ -16,3 +11,4 @@ def result = "ls -ltr".excute()
 println "Result of 'ls -ltr':"
 
 println result.text
+}
