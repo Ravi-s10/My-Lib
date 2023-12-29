1d8453b3abad270f96544e578b39e68564f7f5be
Ravi-s10 <49112786+Ravi-s10@users.noreply.github.com>
2023-12-29 18:31:17 +0530

    Update script.groovy

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,14 +1,7 @@
-#!/usr/bin/env groovy
-
-println "Hello , This is a groovy Script!"
-
 
 def call() {
 
-
 def result = "ls -ltr".excute()
-
-println "Result of 'ls -ltr':"
 
 println result.text
 }
